1b6302f6c11d0c0bc5e5d202b738c4d79240725c
Jessica <jeca.zarate.up@phinmaed.com>
2025-01-29 15:15:11 +0800

    responsive navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,36 +25,36 @@ const pathname = usePathname();
       <header className="sticky inset-x-0 top-0 z-[50]">
         <nav
           aria-label="Global"
-          className="flex items-center justify-between px-6 py-3 lg:px-8 bg-white bg-opacity-50 backdrop-blur-[2px] shadow">
+          className="flex items-center justify-between p-2 sm:px-4 py-2.5 bg-gray-100 md:px-6 md:py-3 lg:px-7 lg:bg-white lg:bg-opacity-50 lg:backdrop-blur-[2px] shadow">
           <div className="flex lg:flex-1">
-            <Link href="/" className="-m-1.5 p-1.5 flex gap-2 text-center align-middle">
+            <Link href="/" className="sm:gap-2 text-center inline-flex items-center hover:bg-gray-300 hover:rounded-md px-2.5 py-1.5">
               <span className="sr-only">Municipality of Calasiao</span>
               <Image
                 alt="sb logo"
                 src="/image/calasiao.png"
-                className="h-11 w-auto ring-2 ring-green-900 rounded-full"
+                className="h-6 sm:h-9 lg:h-10 w-auto ring-1 lg:ring-2 ring-green-900 rounded-full"
                 width={100}
               height={100}
               priority
             />
-            <p className="font-figsemibold text-4xl pt-[3.5px]">SB.</p>
+            <p className="font-figsemibold text-slate-800 text-lg sm:text-2xl lg:text-3xl">SB.</p>
             </Link>
           </div>
           <div className="flex lg:hidden">
             <button
               type="button"
               onClick={() => setMobileMenuOpen(true)}
-              className="inline-flex items-center justify-center rounded-md px-2.5 py-3 text-slate-900">
+              className="inline-flex items-center justify-center rounded-md p-2 text-slate-800 hover:bg-gray-300">
               <span className="sr-only">Open main menu</span>
-              <Bars3Icon aria-hidden="true" className="size-6 " />
+              <Bars3Icon aria-hidden="true" className="size-5 sm:size-6" />
             </button>
           </div>
-          <div className="hidden lg:flex lg:gap-x-1">
+          <div className="hidden lg:flex lg:gap-x-1 lg:shrink-0">
             {navigation.map((item) => (
               <a
                 key={item.name}
                 href={item.href}
-                className={`text-md/6 text-slate-900 hover:shadow font-figregular tracking-wider hover:bg-gray-300 py-3 px-5 hover:rounded-md ${isActive(item.href) ? activeLinkStyles : ""}`}>
+                className={`text-md/6 text-slate-900 hover:shadow font-figregular tracking-wider hover:bg-gray-300 py-2.5 px-4 hover:rounded-md ${isActive(item.href) ? activeLinkStyles : ""}`}>
                 {item.name}
               </a>
             ))}
@@ -65,35 +65,35 @@ const pathname = usePathname();
           onClose={setMobileMenuOpen}
           className="lg:hidden">
           <div className="fixed inset-0 z-100" />
-          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full sm:w-1/2 md:w-1/3 overflow-y-auto p-2 sm:p-4 bg-gray-50 shadow">
             <div className="flex items-center justify-between">
-              <Link href="/" className="-m-1.5 p-1.5">
+              <Link href="/" className="px-2.5 py-1.5 hover:bg-gray-300 rounded-md items-center">
                 <span className="sr-only">Sangguniang Bayan Calasiao</span>
                 <Image
-                  alt="calasiao logo"
-                  src="/image/calasiao.png"
-                  className="h-8 w-auto ring-2 ring-green-900 rounded-full"
-                  width={100}
-                  height={100}
-                  priority
-                />
+                alt="sb logo"
+                src="/image/calasiao.png"
+                className="h-8 sm:h-9 lg:h-11 w-auto ring-1 lg:ring-2 ring-green-900 rounded-full"
+                width={100}
+              height={100}
+              priority
+            />
               </Link>
               <button
                 type="button"
                 onClick={() => setMobileMenuOpen(false)}
-                className="-m-2.5 rounded-md p-2.5 text-gray-700">
+                className="rounded-md text-gray-700 px-2.5 py-1.5 hover:bg-gray-300">
                 <span className="sr-only">Close menu</span>
-                <XMarkIcon aria-hidden="true" className="size-6" />
+                <XMarkIcon aria-hidden="true" className="size-5 sm:size-6" />
               </button>
             </div>
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
+            <div className="flow-root">
+              <div className="mt-4 divide-y divide-gray-500/10">
+                <div>
                   {navigation.map((item) => (
                     <a
                       key={item.name}
                       href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-4 text-base/7 font-figsemibold text-gray-900 hover:bg-gray-200">
+                      className={`block rounded-lg px-3 py-4 font-figsemibold text-gray-900 text-sm sm:text-base hover:bg-gray-200 ${isActive(item.href) ? activeLinkStyles : ""}`}>
                       {item.name}
                     </a>
                   ))}
