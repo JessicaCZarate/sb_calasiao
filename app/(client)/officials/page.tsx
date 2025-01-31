a344b94f4b375d604681e5cb5c7a9dbc0ef589d4
Jessica <jeca.zarate.up@phinmaed.com>
2025-01-31 09:11:54 +0800

    init officials

--- app/(client)/officials/page.tsx
+++ app/(client)/officials/page.tsx
@@ -5,10 +5,19 @@ export default function Page() {
     <section className="h-screen bg-slate-100">
       <h1>Municipal Officials</h1>
       {officials.map((official, index) => (
-        <div key={index} className="flex flex-col gap-4">
-          <h2>{official.name}</h2>
-          <p>{official.position}</p>
-        </div>
+        <ul key={index} className="flex flex-col space-y-2">
+          <li className="flex w-full py-1 sm:w-full">
+            <div className="flex flex-col sm:flex-row w-full items-start sm:items-center">
+              <span className="sm:flex-none font-figmedium text-md text-stone-400 py-1 mb-1 tracking-wider">
+                {official.position}
+              </span>
+              <hr className="flex-grow mx-2 border-t-[0.3px] border-black/20" />
+              <span className="sm:flex-none text-left font-figsemibold sm:text-base break-words text-sm pl-24 sm:pl-0 text-black">
+                {official.name}
+              </span>
+            </div>
+          </li>
+        </ul>
       ))}
     </section>
   );
