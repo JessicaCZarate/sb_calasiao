928be7234347b4f71d32ccf8cb68749e10576546
Jessica <jeca.zarate.up@phinmaed.com>
2025-02-07 09:22:20 +0800

    hero component

--- components/ServicesStrip.tsx
+++ components/ServicesStrip.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const ServicesStrip = () => {
   return (
-    <div className="text-center font-figmedium mt-1">
+    <div className="text-center font-figmedium mt-2 sm:mt-4">
       <div className="font-figregular text-sm sm:text-lg md:text-xl lg:text-2xl [text-wrap:balance] bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 to-60% to-slate-100">
         Providing good governance
         <br /> through{" "}
         <span className="text-yellow-300 inline-flex flex-col h-[calc(theme(fontSize.sm)*theme(lineHeight.tight))] sm:h-[calc(theme(fontSize.lg)*theme(lineHeight.tight))] md:h-[calc(theme(fontSize.xl)*theme(lineHeight.tight))] lg:h-[calc(theme(fontSize.2xl)*theme(lineHeight.tight))] overflow-hidden">
-          <ul className="block animate-text-slide text-left leading-tight [&_li]:block tracking-wider">
+          <ul className="block animate-text-slide text-left leading-tight [&_li]:block tracking-widest">
             <li>Resolutions.</li>
             <li>Ordinances.</li>
             <li>Public Hearings.</li>
--- components/section/Hero.tsx
+++ components/section/Hero.tsx
@@ -25,17 +25,19 @@ const Hero: React.FC<HeroProps> = ({ src, title, services, subtitle }) => {
       <div className="relative isolate px-6 py-32 bg-gradient-to-b from-transparent to-99% to-slate-950 shadow">
         <div className="mx-auto max-w-2xl">
           <div
-            className={`text-center items-center flex flex-col -mt-14 sm:-mt-14 ${
-              !services ? "-mb-24 sm:-mb-0" : ""
+            className={`text-center items-center flex flex-col -mt-10 -mb-16 lg:-mb-5 md:-mb-4 ${
+              !services
+                ? "-mb-32 -mt-[3rem] sm:-mt-[3.5rem] md:-mt-[2.5rem]"
+                : ""
             }`}>
-            <p className="text-lg sm:text-xl md:text-2xl font-figsemibold text-pretty text-gray-300 tracking-wider">
+            <p className="text-lg leading-3 sm:text-xl md:text-[1.4rem] font-figsemibold text-pretty text-gray-300 tracking-wider">
               {subtitle}
             </p>
             <div className="relative inline-block isolate">
-              <h1 className="text-[52px] sm:text-7xl md:text-8xl font-figsemibold tracking-tight text-balance text-transparent bg-gradient-to-r from-yellow-400 to-green-800 bg-clip-text z-10">
+              <h1 className="text-[57px] leading-tight sm:text-7xl md:text-[5.3rem] font-figsemibold tracking-tight text-balance text-transparent bg-gradient-to-r from-yellow-400 to-green-800 bg-clip-text z-10">
                 {title}
               </h1>
-              <h1 className="absolute top-0 text-[52px] sm:text-7xl md:text-8xl font-figsemibold tracking-tight text-balance text-transparent bg-gradient-to-r from-yellow-400 to-green-500 bg-clip-text blur opacity-60">
+              <h1 className="absolute top-0 leading-tight text-[57px] sm:text-7xl md:text-[5.3rem] font-figsemibold tracking-tight text-balance text-transparent bg-gradient-to-r from-yellow-400 to-green-500 bg-clip-text blur opacity-60">
                 {title}
               </h1>
             </div>
