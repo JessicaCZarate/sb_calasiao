0174488b9175206d223f829178fe0b24f697f0f9
Jessica <jeca.zarate.up@phinmaed.com>
2025-02-10 19:18:26 +0800

    supabase db test

--- app/(client)/service/page.tsx
+++ app/(client)/service/page.tsx
@@ -1,3 +1,8 @@
-export default function Page() {
-  return <>Something</>;
+import { createClient } from "@/utils/supabase/server";
+
+export default async function Instruments() {
+  const supabase = await createClient();
+  const { data: instruments } = await supabase.from("instruments").select();
+
+  return <pre>{JSON.stringify(instruments, null, 2)}</pre>;
 }
