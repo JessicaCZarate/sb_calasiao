7f8bc17acab20de4d4908a551816f3802c780333
Jessica <jeca.zarate.up@phinmaed.com>
2025-02-11 00:37:33 +0800

    text-wrap data

--- app/(client)/service/page.tsx
+++ app/(client)/service/page.tsx
@@ -5,7 +5,7 @@ export default async function Documents() {
   const { data: documents } = await supabase.from("documents").select();
 
   return (
-    <pre className="text-xs text-black break-words">
+    <pre className="text-xs text-black break-words flex text-wrap">
       {JSON.stringify(documents)}
     </pre>
   );
