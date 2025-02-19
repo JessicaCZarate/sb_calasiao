ebba7fa9ff577a1dd3cfcf4cdf59f1a8a07a7d72
Jessica <jeca.zarate.up@phinmaed.com>
2025-02-20 07:45:43 +0800

    vision mission data

--- app/(client)/about/page.tsx
+++ app/(client)/about/page.tsx
@@ -7,8 +7,25 @@ export default function Page() {
         src="about"
         title="About Us"
         services={false}
-        subtitle="Secretariat Services and Personnel"
+        subtitle="Secretariat Services"
       />
+      <div>
+        <h2>Vision</h2>
+        <p>
+          A dynamic responsive and effective Sangguniang Bayan Secretariat
+          guided by the Divine Providence with the firm commitment in the
+          ultimate priority of the general welfare of the people og Calasiao,
+          Pangasinan through quality legislation, to make Calasiao, Pangasinan
+          as the best place to live, invest, work, and raise a family.
+        </p>
+      </div>
+      <div>
+        <h2>Mission</h2>
+        <p>
+          To ensure efficient and effective delivery of administrative and
+          Secretariat services of the Sangguniang Bayan of Calasiao, Pangasinan.
+        </p>
+      </div>
     </section>
   );
 }
