830cab2f0ee32e6c9d9471d34aa549e65d5bf3f6
Jessica <jeca.zarate.up@phinmaed.com>
2025-02-19 23:09:21 +0800

    officials images except sir sec

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -9,10 +9,7 @@ export default function Layout({
   return (
     <>
       <div className="hidden sm:flex sm:bg-red-600 md:flex md:bg-yellow-400 lg:flex lg:bg-green-700 justify-center w-full bg-gray-100">
-        <Badge
-          text="Sangguniang Bayan of Calasiao 2025"
-          href="/municipal-officials"
-        />
+        <Badge text="Sangguniang Bayan of Calasiao 2025" href="/officials" />
       </div>
       <main className="relative bg-gray-100 h-full">
         <Navbar />
--- app/lib/officials.ts
+++ app/lib/officials.ts
@@ -2,74 +2,62 @@ export const officials = [
   {
     name: "KEVIN ROY Q. MACANLALAY",
     position: "Mayor",
-    image:
-      "https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp",
+    image: "/image/M. HON. KEVIN ROY Q. MACANLALAY.PNG",
   },
   {
     name: "NESTOR A. GABRILLO ",
     position: "Vice Mayor",
-    image:
-      "https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp",
+    image: "/image/VM. HON. NESTOR A. GABRILLO.PNG",
   },
   {
     name: "MANNY V. DATUIN",
     position: "Councilor",
-    image:
-      "https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp",
+    image: "/image/HON. MANNY V. DATUIN.PNG",
   },
   {
     name: "FELIPE K. DE VERA",
     position: "Councilor",
-    image:
-      "https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp",
+    image: "/image/HON. FELIPE K. DE VERA.PNG",
   },
   {
     name: "ARDIESON B. SORIANO",
     position: "Councilor",
-    image:
-      "https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp",
+    image: "/image/HON. ARDIESON B. SORIANO.PNG",
   },
   {
     name: "HAVERDANI DAS B. MESINA",
     position: "Councilor",
-    image:
-      "https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp",
+    image: "/image/HON. HAVERDANI DAS B. MESINA.PNG",
   },
   {
     name: "GERALD C. AFICIAL",
     position: "Councilor",
-    image:
-      "https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp",
+    image: "/image/HON. GERALD C. AFICIAL.PNG",
   },
   {
     name: "ELIAS S. VILLANUEVA",
     position: "Councilor",
-    image:
-      "https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp",
+    image: "/image/HON. ELIAS S. VILLANUEVA.PNG",
   },
   {
     name: "MYC D. SISON",
     position: "Councilor",
-    image:
-      "https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp",
+    image: "/image/HON. MYC D. SISON.PNG",
   },
   {
     name: "JOSE A. LORESCO",
     position: "Councilor",
-    image:
-      "https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp",
+    image: "/image/HON. JOSE A. LORESCO.PNG",
   },
   {
     name: "PATRICK A. CARAMAT",
     position: "Liga President",
-    image:
-      "https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp",
+    image: "/image/HON. PATRICK A. CARAMAT.PNG",
   },
   {
     name: "NARAYANA RSI DAS S. MESINA",
     position: "SK Federation President",
-    image:
-      "https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp",
+    image: "/image/HON. NARAYANA RSI DAS S. MESINA.PNG",
   },
   {
     name: "BRYAN B. CALAUNAN",
