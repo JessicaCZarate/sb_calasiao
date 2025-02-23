8961a2bf57aef1d6aef01512112b2a32fe51f7bf
Jessica <jeca.zarate.up@phinmaed.com>
2025-02-23 17:07:47 +0800

    carousel with position and name

--- components/VImgScroll.tsx
+++ components/VImgScroll.tsx
@@ -10,16 +10,26 @@ const VImgScroll: React.FC<OfficialsProps> = ({ officials }) => {
   const sortedData = officials.sort((a, b) => a.id - b.id);
   return (
     <div className="flex w-full justify-center bg-[url('/image/hhall.JPG')] bg-cover bg-center bg-no-repeat shadow rounded-sm bg-blend-multiply bg-gray-500 items-center py-5 mt-4 lg:mt-0">
-      <div className="carousel carousel-vertical rounded-lg h-72 sm:h-96">
+      <div className="carousel carousel-vertical rounded-lg h-80 sm:h-96">
         {sortedData.slice(0, -1).map((official) => (
-          <div key={official.id} className="carousel-item h-full w-auto">
-            <Image
-              src={official.image}
-              alt={official.name}
-              className="object-fill h-72 sm:h-96 w-full max-w-44 sm:max-w-none"
-              width={500}
-              height={800}
-            />
+          <div key={official.id} className="carousel-item h-full w-full">
+            <div className="relative justify-self-center w-full shadow">
+              <Image
+                alt={`${official.name}`}
+                src={official.image}
+                width={500}
+                height={800}
+                className="rounded-md h-80 sm:h-96 w-full object-fill"
+              />
+              <div className="absolute bottom-0 left-0 text-left w-full rounded-b-md bg-gradient-to-t from-slate-800/70 to-transparent ">
+                <h3 className="pl-2 text-white text-base font-figmedium">
+                  {official.name}
+                </h3>
+                <p className="pl-2 pb-1 text-xs text-gray-300 font-figregular">
+                  <i>{official.position}</i>
+                </p>
+              </div>
+            </div>
           </div>
         ))}
       </div>
